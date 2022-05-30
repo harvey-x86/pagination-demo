2f369c9f4bf431761628b06f29730db160769cd1
Harvey <70911622+harvey-x86@users.noreply.github.com>
2022-05-31 00:18:20 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    $curPage = intval($_GET["current"]) ?? 0;
+    
+    $prev = ($curPage <= 0) ? $curPage : ($curPage - 2);
+    $next = $curPage + 2;
+?>
+<html>
+
+    <body>
+        <h1>HOPE THIS HELPS - HARVEY :)</h1>
+        <table>
+            <tr><th>Full Name</th><th>Email Address</th></tr>
+            <?php
+                $connection = new mysqli("92.204.220.200", "AdminHarvey", "c9Ry53#]8&$%\$a", "hvycmb");
+                
+                $q = $connection -> query("SELECT fullname, email FROM ash_test LIMIT 2 OFFSET {$curPage}");
+                
+                while ($row = $q -> fetch_assoc()) {
+                    echo "<tr><td>{$row['fullname']}</td><td>{$row['email']}</td></tr>";
+                }
+                
+                echo "</table><a href=\"http://harveycoombs.com/tetsing/?current={$prev}\">Prev.</a><a href=\"http://harveycoombs.com/tetsing/?current={$next}\">Next</a>";
+            ?>
+    </body>
+
+</html>
